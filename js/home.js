83355cbe43def9cd2ecd1042f935e01fecb6b087
Mohamed-Almarag <mohamedalmarag97@gmail.com>
2021-03-30 17:36:15 +0200

    finish first task

---
+++ js/home.js
@@ -0,0 +1,141 @@
+
+
+
+
+
+
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  dots: false,
+  arrows: false,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3,
+  //       dots: false,
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1,
+  //       dots: false,
+  //     }
+  //   }
+  // ]
+});
+
+
+$('.center-products').slick({
+  // centerMode: true,
+  // centerPadding: '60px',
+  // slidesToShow: 6,
+  // dots: true,
+
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  arrows: false,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(() => {
+//   $(".your-class").slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//           infinite: true,
+//           dots: true,
+//         },
+//       },
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+//       {
+//         breakpoint: 576,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+      
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//   });
+// });
+
+
+
+
+
+
+
+
+// //  $('.your-class').slick({
+// //   slidesToShow: 1,
+// //   slidesToScroll: 1,
+// //   arrows: false,
+// //   fade: true,
+// //   asNavFor: '.slider-nav'
+// // });
+// // $('.slider-nav').slick({
+// //   slidesToShow: 3,
+// //   slidesToScroll: 1,
+// //   asNavFor: '.slider-for',
+// //   dots: true,
+// //   centerMode: true,
+// //   focusOnSelect: true
+// // });
