c1f5173dfc4663d9dbbb8cf15d65526105222756
Mohamed-Almarag <mohamedalmarag97@gmail.com>
2021-04-01 00:48:33 +0200

    second task

---
+++ js/sizes/sizes.js
@@ -0,0 +1,31 @@
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 2,
+  dots: true,
+  arrows: false,
+
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3,
+  //       dots: false,
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1,
+  //       dots: false,
+  //     }
+  //   }
+  // ]
+});
