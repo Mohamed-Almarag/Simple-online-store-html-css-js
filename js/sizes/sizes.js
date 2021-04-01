274a7e7b27698117075414573beca83329481c14
Mohamed-Almarag <mohamedalmarag97@gmail.com>
2021-04-01 14:49:06 +0200

    finish second task

--- js/sizes/sizes.js
+++ js/sizes/sizes.js
@@ -6,7 +6,39 @@ $('.center').slick({
   dots: true,
   arrows: false,
 
-  // responsive: [
+  responsive: [
+    // {
+    //   breakpoint: 768,
+    //   settings: {
+    //     arrows: false,
+    //     centerMode: true,
+    //     centerPadding: '40px',
+    //     slidesToShow: 3,
+    //     dots: false,
+    //   }
+    // },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1,
+        dots: true,
+      }
+    }
+  ]
+});
+
+
+$('.related-products-slider').slick({
+  centerMode: true,
+  // centerPadding: '60px',
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  dots: false,
+  arrows: false,
+    // responsive: [
   //   {
   //     breakpoint: 768,
   //     settings: {
